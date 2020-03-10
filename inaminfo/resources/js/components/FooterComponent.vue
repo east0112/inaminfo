bca2b95f3ced8884160ad5ba189ef67645f9f9d6
east_outside <east_outside@example.com>
2020-03-10 12:09:12 +0900

    project build

---
+++ inaminfo/resources/js/components/FooterComponent.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="footer">
+    <div class="footer__content">
+      <div class="footer__footer-desc">
+        <p>当サイトは非公式ファンサイトです。</p>
+        <i class="fab fa-twitter link-icon--twitter"></i>
+      </div>
+      <div class="footer__footer-link">
+        <a  href="http://smavoice.jp/" target="_blank"><p class="link-icon--outside">SMA VOICE</p></a>
+        <a  href="https://anjuinami.com/" target="_blank"><p class="link-icon--outside">伊波杏樹 Official Site</p></a>
+        <a  href="https://lineblog.me/anju_inami/" target="_blank"><p class="link-icon--outside">伊波杏樹 公式ブログ</p></a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function(){
+    return{
+        }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ inaminfo/resources/views/view.blade.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="ja">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="csrf-token" content="{{ csrf_token() }}">
+<meta name="description" content="伊波杏樹さんの出演情報をまとめた非公式ファンサイトです。"/>
+<meta name="keywords" content="SMA声優, SMA VOICE,ファンクラブ">
+
+<meta name="csrf-token" content="{{ csrf_token() }}">
+<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/hover.css/2.1.1/css/hover.css">
+<link rel="stylesheet" href="{{ mix('/css/app.css') }}">
+
+<head>
+    <meta charset="UTF-8">
+    <title>inaminfo</title>
+</head>
+<body>
+<div id="app">
+    <header-component></header-component>
+    <example-component></example-component>
+    <footer-component></footer-component>
+</div>
+</body>
+<script src=" {{ mix('js/app.js') }} "></script>
