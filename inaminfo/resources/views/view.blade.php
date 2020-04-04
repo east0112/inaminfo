3b3d41fdf1ab97b1091917434fd9738d992a1c41
豊島大地 <toyoshimadaichi@Macbook-for-east.local>
2020-04-04 23:35:14 +0900

    ブラウザキャッシュの無効化

--- inaminfo/resources/views/view.blade.php
+++ inaminfo/resources/views/view.blade.php
@@ -4,9 +4,10 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="csrf-token" content="{{ csrf_token() }}">
 <meta name="description" content="伊波杏樹さんの出演情報をまとめた非公式ファンサイトです。"/>
-<meta name="keywords" content="伊波杏樹, SMA声優, SMA VOICE,ファンクラブ">
+<meta name="keywords" content="伊波杏樹, SMA声優 ,ファンクラブ">
 <meta http-equiv="Pragma" content="no-cache">
-
+<meta http-equiv="Cache-Control" content="no-cache">
+<meta http-equiv="Expires" content="0">
 <meta name="csrf-token" content="{{ csrf_token() }}">
 <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
